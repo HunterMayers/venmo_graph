fa00e7d53c8104c5c3a83d8d1578e195f5174941
HunterMayers <huntercmayers@gmail.com>
2018-08-03 12:06:14 -0700

    uploading

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # venmo_graph
+After listening to Security Now! episode #673 which discusses Venmo's public transaction log. I thought it would be fun to look into. Given the nature of the data I wanted to create a large graph.
+I wrote a quick python script to grab data from the api, used the data to create a graph(nodes being people and edges being transactions), and then export the data to a file in gefx(Graph Exchange XML format).
+
+The problem I ran into was that venmo's api only allows 2 requests a minute each request containing about 20 transactions. This meant that gathering enough data to see any real connectivity in the graph would take too long.
+
+Here is an image of the graph generated after a few queries to the api
+![screenshot](img/Capture.png "an image of the graph")
---
+++ venmo.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import networkx as nx
+import time
+class Person:
+  def __init__(self, first_name, last_name, internal_id):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.internal_id = internal_id
+G = nx.DiGraph()
+seen_people = set()
+num_req = 0
+page = "https://venmo.com/api/v5/public"
+try:
+  while num_req < 250:
+    response = requests.get(page)
+    if response.status_code == 200:
+      num_req += 1
+      info = json.loads(response.content)
+      #print(info['paging']['previous'])
+      page = info['paging']['previous']
+      for d in info['data']:
+        sender = d['actor']
+        receiver = d['transactions']
+        s = Person(sender['firstname'], sender['lastname'], sender['id'])
+        for m in receiver:
+          rec = m['target']
+          if isinstance(rec, dict):
+            r = Person(rec['firstname'], rec['lastname'], rec['id'])
+          else:
+            r = Person('invalid', 'invalid', 'invalid')
+        if d['type'] == 'payment':
+          if s.internal_id not in seen_people:
+            seen_people.add(s.internal_id)
+            G.add_node(s.internal_id, name=s.first_name + ' ' + s.last_name)
+          if r.internal_id not in seen_people:
+            seen_people.add(r.internal_id)
+            G.add_node(r.internal_id, name=r.first_name + ' ' + r.last_name)
+          G.add_edge(s.internal_id, r.internal_id, message=d['message'], date=d['created_time'])
+        else:
+          if s.internal_id not in seen_people:
+            seen_people.add(s.internal_id)
+            G.add_node(s.internal_id, name=s.first_name + ' ' + s.last_name)
+          if r.internal_id not in seen_people:
+            seen_people.add(r.internal_id)
+            G.add_node(r.internal_id, name=r.first_name + ' ' + r.last_name)
+          G.add_edge(r.internal_id, s.internal_id, message=d['message'], date=d['created_time'])
+    else:
+      print(response.status_code)
+      print('number of requests completed: %d' % num_req)
+      print('current page: ' + page)
+      time.sleep(15)
+
+except KeyboardInterrupt:
+  print(num_req)
+  nx.write_gexf(G, "new.gexf")
+  
+
